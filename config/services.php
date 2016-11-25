7341a8ceec49161525e32f9f5e3c63d389075f65
hellenbarrantes <hellen.selenia@gmail.com>
2016-11-24 21:55:07 -0600

    avance2

--- config/services.php
+++ config/services.php
@@ -34,5 +34,25 @@ return [
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
+    
+      'facebook' => [
+        'client_id' => '646056505555602',
+        'client_secret' => 'cdaceaaf9c40409138f434a6f7f234be',
+        'redirect' =>  'http://localhost:8000/callback/facebook',
+    ],
+    
+    'twitter' => [
+    'client_id' => 'qTBuCWl0bExeq3rs7UObuIFk1', // configure with your app id
+    'client_secret' => 'xZzqgspwCa1paxVSND1gO0PxnjFr3mSluIIOgPFu83ldIZhMtA', // your app secret
+    'redirect' => 'http://localhost:8080/callback/twitter', // leave blank for now
+    ],
+    
+    'google' => [
+    'client_id' => '393094122079-mb6olmk0lh1iivpu301n9e0jp1lgr0cc.apps.googleusercontent.com', // configure with your app id
+    'client_secret' => 'L7oyTQk8OO6DDPsPIyy2-TAu', // your app secret
+    'redirect' => 'http://localhost:8000/callback/google', // leave blank for now
+    ],
+    
+
 
 ];
